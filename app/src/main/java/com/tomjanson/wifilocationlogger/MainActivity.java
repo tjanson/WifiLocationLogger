82275d1b44310e70b1c16d7d96d3ac040d5e8c97
Tom Janson <tom.janson@rwth-aachen.de>
2015-03-10 17:27:21 +0100

    initial commit

---
+++ app/src/main/java/com/tomjanson/wifilocationlogger/MainActivity.java
@@ -0,0 +1,293 @@
+package com.tomjanson.wifilocationlogger;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.IntentFilter;
+import android.location.Location;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import android.net.wifi.WifiManager;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/*
+ * Location update code based on:
+ *     https://developer.android.com/training/location/receive-location-updates.html
+ * Wifi scan code loosely based on:
+ *     http://www.tutorialspoint.com/android/android_wi_fi.htm
+ *     https://github.com/Skarbo/WifiMapper
+ */
+
+public class MainActivity extends Activity implements
+        ConnectionCallbacks, OnConnectionFailedListener, LocationListener {
+
+    // Logback loggers, see https://github.com/tony19/logback-android
+    Logger log;
+    Logger diskLog;
+
+    // Location update intervals
+    // it seems the updates take at least 5s; setting it lower doesn't seem to work
+    static final long LOCATION_UPDATE_INTERVAL_MILLIS = 3000;
+    static final long FASTEST_LOCATION_UPDATE_INTERVAL_MILLIS = 1000;
+
+    // Wifi scan delay (i.e., wait $delay between completion of scan and start of next scan)
+    static final long WIFI_SCAN_DELAY_MILLIS = 2000;
+
+    // TODO: try different delays
+
+    // Instance state Bundle keys,
+    // see https://developer.android.com/training/basics/activity-lifecycle/recreating.html
+    private final static String LOCATION_KEY                         = "location-key";
+    private final static String LAST_LOCATION_UPDATE_TIME_STRING_KEY = "last-location-update-time-string-key";
+    private final static String LAST_WIFI_SCAN_TIME_STRING_KEY       = "last-wifi-scan-time-string-key";
+
+    // Used to access Fused Location API,
+    // see https://developer.android.com/google/play-services/location.html
+    private GoogleApiClient googleApiClient;
+    private LocationRequest locationRequest;
+    Location currentLocation;
+
+    // Wifi scan stuff
+    static WifiManager           wifiManager;
+    static IntentFilter          wifiIntentFilter;
+    static WifiBroadcastReceiver wifiBroadcastReceiver;
+
+    // toggles logging location+wifi to file
+    // debug logs may be created regardless of this
+    boolean logToFile = false;
+
+    // UI Elements
+    Button   loggingButton;
+    TextView locationTV;
+    TextView locationAccuracyTV;
+    TextView locationUpdateTV;
+    Date     lastLocationUpdateTime;
+    TextView wifiTV;
+    EditText wifiFilterET;
+    TextView wifiUpdateTV;
+    String   wifiListString;
+    Date     lastWifiScanTime;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        log = LoggerFactory.getLogger(MainActivity.class); // for debugging
+        diskLog = LoggerFactory.getLogger("disk");         // for machine-readable log
+        log.debug("Started; " + Build.VERSION.RELEASE + ", " + Build.ID + ", " + Build.MODEL);
+
+        setContentView(R.layout.activity_main);
+
+        assignUiElements();
+
+        updateValuesFromBundle(savedInstanceState);
+
+        buildGoogleApiClient();
+
+        initWifiScan();
+        wifiManager.startScan();
+    }
+
+    private void assignUiElements() {
+        loggingButton       = (Button)   findViewById(R.id.loggingButton);
+        locationTV          = (TextView) findViewById(R.id.locationTextView);
+        locationAccuracyTV  = (TextView) findViewById(R.id.locationAccuracyTextView);
+        locationUpdateTV    = (TextView) findViewById(R.id.locationUpdateTextView);
+        wifiTV              = (TextView) findViewById(R.id.wifiTextView);
+        wifiFilterET        = (EditText) findViewById(R.id.wifiFilterEditText);
+        wifiUpdateTV        = (TextView) findViewById(R.id.wifiUpdateTextView);
+    }
+
+    void updateUI() {
+        if (currentLocation != null) {
+            locationTV.setText(currentLocation.getLatitude() + ", " + currentLocation.getLongitude());
+            locationAccuracyTV.setText(currentLocation.getAccuracy() + " m");
+        }
+        if (lastLocationUpdateTime != null) {
+            locationUpdateTV.setText(DateFormat.getTimeInstance().format(lastLocationUpdateTime));
+        }
+        if (lastWifiScanTime != null) {
+            wifiUpdateTV.setText(DateFormat.getTimeInstance().format(lastWifiScanTime));
+        }
+        if (wifiListString != null) {
+            wifiTV.setText(wifiListString);
+        }
+
+        if (logToFile) {
+            loggingButton.setText(R.string.logging_stop);
+        } else {
+            loggingButton.setText(R.string.logging_start);
+        }
+    }
+
+    public void toggleLogging(View view) {
+        logToFile = !logToFile;
+        updateUI();
+    }
+
+    private void initWifiScan() {
+        wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        wifiBroadcastReceiver = new WifiBroadcastReceiver(this);
+        wifiIntentFilter = new IntentFilter();
+        wifiIntentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
+        this.registerReceiver(wifiBroadcastReceiver, wifiIntentFilter);
+        log.debug("Registered wifiBroadcastReceiver");
+    }
+
+    private void updateValuesFromBundle(Bundle savedInstanceState) {
+        log.debug("Updating values from bundle");
+        if (savedInstanceState != null) {
+            if (savedInstanceState.keySet().contains(LOCATION_KEY)) {
+                currentLocation = savedInstanceState.getParcelable(LOCATION_KEY);
+            }
+            if (savedInstanceState.keySet().contains(LAST_LOCATION_UPDATE_TIME_STRING_KEY)) {
+                lastLocationUpdateTime = new Date(savedInstanceState.getLong(LAST_LOCATION_UPDATE_TIME_STRING_KEY));
+            }
+            if (savedInstanceState.keySet().contains(LAST_WIFI_SCAN_TIME_STRING_KEY)) {
+                lastWifiScanTime = new Date(savedInstanceState.getLong(LAST_WIFI_SCAN_TIME_STRING_KEY));
+            }
+            updateUI();
+        }
+    }
+
+    synchronized void buildGoogleApiClient() {
+        log.trace("Building GoogleApiClient");
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        createLocationRequest();
+    }
+
+    void createLocationRequest() {
+        locationRequest = new LocationRequest();
+
+        // Sets the desired interval for active location updates. This interval is
+        // inexact. You may not receive updates at all if no location sources are available, or
+        // you may receive them slower than requested. You may also receive updates faster than
+        // requested if other applications are requesting location at a faster interval.
+        locationRequest.setInterval(LOCATION_UPDATE_INTERVAL_MILLIS);
+
+        // Sets the fastest rate for active location updates. This interval is exact, and your
+        // application will never receive updates faster than this value.
+        locationRequest.setFastestInterval(FASTEST_LOCATION_UPDATE_INTERVAL_MILLIS);
+
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    /**
+     * Requests location updates from the FusedLocationApi.
+     */
+    void startLocationUpdates() {
+        // The final argument to {@code requestLocationUpdates()} is a LocationListener
+        // (http://developer.android.com/reference/com/google/android/gms/location/LocationListener.html).
+        LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this);
+    }
+
+    void stopLocationUpdates() {
+        LocationServices.FusedLocationApi.removeLocationUpdates(googleApiClient, this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        log.info("Location: {}", location);
+        currentLocation = location;
+        lastLocationUpdateTime = new Date();
+        updateUI();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        googleApiClient.connect();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (googleApiClient.isConnected()) {
+            startLocationUpdates();
+        }
+        this.registerReceiver(wifiBroadcastReceiver, wifiIntentFilter);
+        log.debug("Registered wifiBroadcastReceiver");
+        wifiManager.startScan();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        this.unregisterReceiver(wifiBroadcastReceiver);
+        log.debug("Unregistered wifiBroadcastReceiver");
+        if (googleApiClient.isConnected()) {
+            stopLocationUpdates();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (googleApiClient.isConnected()) {
+            googleApiClient.disconnect();
+        }
+    }
+
+    /**
+     * Runs when a GoogleApiClient object successfully connects.
+     */
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        log.info("Connected to GoogleApiClient");
+
+        if (currentLocation == null) {
+            currentLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+            lastLocationUpdateTime = new Date();
+            updateUI();
+        }
+
+        startLocationUpdates();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        log.warn("Connection failed: ConnectionResult.getErrorCode() = {}", result.getErrorCode());
+    }
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        log.info("Connection suspended");
+        googleApiClient.connect();
+    }
+    /**
+     * Stores activity data in the Bundle.
+     */
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle savedInstanceState) {
+        savedInstanceState.putParcelable(LOCATION_KEY, currentLocation);
+        if (lastLocationUpdateTime != null) {
+            savedInstanceState.putLong(LAST_LOCATION_UPDATE_TIME_STRING_KEY, lastLocationUpdateTime.getTime());
+        }
+        if (lastWifiScanTime != null) {
+            savedInstanceState.putLong(LAST_WIFI_SCAN_TIME_STRING_KEY, lastWifiScanTime.getTime());
+        }
+        super.onSaveInstanceState(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/tomjanson/wifilocationlogger/WifiBroadcastReceiver.java
@@ -0,0 +1,98 @@
+package com.tomjanson.wifilocationlogger;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.ScanResult;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+/*
+ * Receives Wifi scan result whenever WifiManager has them,
+ * updates `wifiListString` and `lastWifiScanTime`,
+ * logs location (and accuracy) and Wifis (SSID, BSSID, strength) to disk
+ */
+class WifiBroadcastReceiver extends BroadcastReceiver {
+    private final MainActivity m;
+
+    private final Comparator<ScanResult> RSSI_ORDER =
+            new Comparator<ScanResult>() {
+                public int compare(ScanResult e1, ScanResult e2) {
+                    return Integer.compare(e2.level, e1.level);
+                }
+            };
+
+    private static final String WIFI_SCAN_TIMER = "wifi-scan-timer";
+    private static Timer wifiScanTimer;
+
+    public WifiBroadcastReceiver(MainActivity m) {
+        this.m = m;
+        wifiScanTimer = new Timer(WIFI_SCAN_TIMER);
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        List<ScanResult> scanResultList = MainActivity.wifiManager.getScanResults();
+        m.lastWifiScanTime = new Date();
+
+        Collections.sort(scanResultList, RSSI_ORDER);
+        m.log.debug("Wifis: {}", scanResultList);
+
+        String combined = "";
+        Pattern filter = makeFilter();
+
+        for (ScanResult wifi : scanResultList) {
+            if (!filter.matcher(wifi.SSID).matches()) {
+                continue;
+            }
+
+            combined += wifi.SSID + " [" + wifi.BSSID + "]" + ": " + wifi.level + "\n";
+            log(wifi);
+        }
+
+        m.wifiListString = combined;
+        m.updateUI();
+
+        // schedule next scan after short delay
+        wifiScanTimer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    MainActivity.wifiManager.startScan();
+                }
+            }, MainActivity.WIFI_SCAN_DELAY_MILLIS);
+    }
+
+    private void log(ScanResult wifi) {
+        if (m.logToFile) {
+            m.diskLog.info(m.currentLocation.getLatitude()
+                 + ";" + m.currentLocation.getLongitude()
+                 + ";" + m.currentLocation.getAccuracy()
+                 + ";" + m.lastLocationUpdateTime.getTime()
+                 + ";" + wifi.SSID
+                 + ";" + wifi.BSSID
+                 + ";" + wifi.level
+                 + ";" + m.lastWifiScanTime.getTime());
+        }
+    }
+
+    private Pattern makeFilter() {
+        // if not a valid regular expression or empty, don't filter at all
+        String regexp = m.wifiFilterET.getText().toString();
+        if (regexp.equals("")) {
+            regexp = ".*";
+        }
+        try {
+            return Pattern.compile(regexp);
+        } catch (PatternSyntaxException ex) {
+            return Pattern.compile(".*");
+        }
+    }
+
+}
