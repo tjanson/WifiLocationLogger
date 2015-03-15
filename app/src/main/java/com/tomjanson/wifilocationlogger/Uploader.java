0c4c6d1f834faae1a272e532096b836aa927fca9
Tom Janson <tom.janson@rwth-aachen.de>
2015-03-15 15:59:39 +0100

    don't attempt to upload if log file is not found

--- app/src/main/java/com/tomjanson/wifilocationlogger/Uploader.java
+++ app/src/main/java/com/tomjanson/wifilocationlogger/Uploader.java
@@ -32,28 +32,28 @@ class Uploader {
         RequestParams params = new RequestParams();
         try {
             params.put(targetFilename, file);
+
+            post(m.uploadUrlET.getText().toString(), params, new AsyncHttpResponseHandler() {
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                    Toast.makeText(m, m.getString(R.string.upload_successful), Toast.LENGTH_LONG).show();
+                    m.log.info("Upload success, statusCode: " + statusCode);
+                    //m.onUploadSuccess(statusCode, headers, responseBody);
+                }
+
+                @Override
+                public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                    Toast.makeText(m, m.getString(R.string.upload_failed), Toast.LENGTH_LONG).show();
+                    m.log.warn("Upload failure, statusCode: " + statusCode);
+                    if (error != null) {
+                        m.log.error("Throwable error = {}", error);
+                    }
+                    //m.onUploadFailure(statusCode, headers, responseBody, error);
+                }
+            });
         } catch(FileNotFoundException e) {
             Toast.makeText(m, m.getString(R.string.upload_file_not_found), Toast.LENGTH_LONG).show();
             m.log.error("FileNotFoundException e = {}", e);
         }
-
-        post(m.uploadUrlET.getText().toString(), params, new AsyncHttpResponseHandler() {
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-                Toast.makeText(m, m.getString(R.string.upload_successful), Toast.LENGTH_LONG).show();
-                m.log.info("Upload success, statusCode: " + statusCode);
-                //m.onUploadSuccess(statusCode, headers, responseBody);
-            }
-
-            @Override
-            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-                Toast.makeText(m, m.getString(R.string.upload_failed), Toast.LENGTH_LONG).show();
-                m.log.warn("Upload failure, statusCode: " + statusCode);
-                if (error != null) {
-                    m.log.error("Throwable error = {}", error);
-                }
-                //m.onUploadFailure(statusCode, headers, responseBody, error);
-            }
-        });
     }
 }
